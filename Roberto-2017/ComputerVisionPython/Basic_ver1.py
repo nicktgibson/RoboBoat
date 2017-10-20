9f7c9a3cab3d9cc25b4b82c9ffa3c4170a331d57
Roberto <rowrowroboboat@gmail.com>
2017-10-20 16:01:04 -0400

    c

--- Roberto-2017/ComputerVisionPython/Basic_ver1.py
+++ Roberto-2017/ComputerVisionPython/Basic_ver1.py
@@ -1,3 +1,47 @@
 # FSU College of Engineering - AUVSI RoboBoat Competition
 # Written by Nicholas Gibson on 10/20/17
 
+import cv2
+import sys
+
+if __name__ == '__main__':
+
+    # Read video
+    video = cv2.VideoCapture("videos/chaplin.mp4")
+
+    # Exit if video not opened.
+    if not video.isOpened():
+        print "Could not open video"
+        sys.exit()
+
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print 'Cannot read video file'
+        sys.exit()
+
+
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
+
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+
+        # Draw bounding box
+        if ok:
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (0, 0, 255))
+
+        # Display result
+        cv2.imshow("Tracking", frame)
+
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27: break
