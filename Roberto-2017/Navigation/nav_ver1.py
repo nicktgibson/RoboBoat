f39073683edf8ec2e8b03f0be456bbfc65d21276
nicktgibson <ntg15b@my.fsu.edu>
2017-12-13 17:25:34 -0500

    Rename graphing

--- Roberto-2017/Navigation/nav_ver1.py
+++
@@ -1,7 +0,0 @@
-# FSU College of Engineering - AUVSI RoboBoat Competition
-# Written by Nicholas Gibson on 12/13/2017
-
-import matplotlib.pyplot as plt
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
-plt.show()
