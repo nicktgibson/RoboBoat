d40d1e24af753115123ccfee6419b76514ca9703
nicktgibson <ntg15b@my.fsu.edu>
2017-12-13 17:22:51 -0500

    Testing matplotlib

--- Roberto-2017/Navigation/nav_ver1.py
+++ Roberto-2017/Navigation/nav_ver1.py
@@ -1,2 +1,7 @@
-# 12-12-17
+# FSU College of Engineering - AUVSI RoboBoat Competition
+# Written by Nicholas Gibson on 12/13/2017
 
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
