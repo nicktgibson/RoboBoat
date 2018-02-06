26dea5c8e1f4775edf055b7ed6e2070d662c7052
nicktgibson <ntg15b@my.fsu.edu>
2018-02-06 18:04:58 -0500

    Added reference

---
+++ Roberto-2017/reference/PythonSerial/Serial_ver1.py
@@ -0,0 +1,35 @@
+import serial
+import time
+
+
+"""
+class serial.Serial
+
+
+__init__(port="/dev/ttyUSB0", baudrate=9600, bytesize=EIGHTBITS, parity=PARITY_NONE, stopbits=STOPBITS_ONE, timeout=None,
+         xonxoff=False, rtscts=False, write_timeout=None, dsrdtr=False, inter_byte_timeout=None, exclusive=None)
+"""
+ser = serial.Serial('/dev/ttyACM0', 9600)  # open serial port
+print(ser.name)
+#r = ser.readline()
+#print(r)
+
+while True:
+
+    s = raw_input("Command: ")
+
+
+    if s == 'z':
+        ser.write("+000+000+000+000>")
+        time.sleep(1)
+        ser.close()
+        break
+    elif: s== 'w'
+
+    elif: s == 'a'
+
+    elif: s == 'd'
+
+    else:
+        ser.write(s)
+
