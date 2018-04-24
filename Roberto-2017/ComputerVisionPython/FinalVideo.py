ad1d0f6d6d9f91898d0ca612a5bf40e1c7a63bc5
nicktgibson <ntg15b@my.fsu.edu>
2018-04-24 15:48:53 -0400

    New things from Linux side, Nick Computer

---
+++ Roberto-2017/ComputerVisionPython/FinalVideo.py
@@ -0,0 +1,1 @@
+# Written by Nicholas Gibson on 4/24/18
--- Roberto-2017/ComputerVisionPython/FindCenterBuoys.py
+++ Roberto-2017/ComputerVisionPython/FindCenterBuoys.py
@@ -1,3 +1,5 @@
+# Written by Nicholas Gibson on 4/10/18
+
 # Written by Nicholas Gibson on 2/14/18
 
 # import the necessary packages
@@ -7,6 +9,14 @@ import argparse
 import imutils
 import cv2
 
+# setting up text
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (10, 440)
+fontScale = 0.75
+fontColor = (0, 0, 255)
+lineType = 1
+
+bottomLeftCornerOfTextRed = (140, 440)
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -18,26 +28,26 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-greenLower = (20, 86, 70)
-greenUpper = (40, 200, 180)
+#greenLower = (20, 86, 70)
+#greenUpper = (40, 200, 200)
 
-greenLower = (20, 86, 70)
-greenUpper = (40, 210, 210)
+#green = (45, 180, 175)
+green = (156, 170, 46) # BGR
+#print(cv2.cvtColor((green), cv2.COLOR_BGR2HSV))
+
+greenLower = (35, 160, 100)
+greenUpper = (50, 200, 255)
+
+red = (71, 70, 190)
 
 # lower mask (0-10) for Red
 lower_redA = np.array([0, 50, 70])
 upper_redA = np.array([7, 255, 200])
-lower_redA = np.array([0, 50, 70])
-upper_redA = np.array([1, 51, 71])
 
 
 # upper mask (170-180) Red
 lower_redB = np.array([172, 50, 70])
 upper_redB = np.array([180, 255, 200])
-
-lower_redB = np.array([172, 50, 70])
-upper_redB = np.array([173, 51, 71])
-
 
 ptsRed = deque(maxlen=args["buffer"])
 ptsGreen = deque(maxlen=args["buffer"])
@@ -51,10 +61,17 @@ if not args.get("video", False):
 else:
     camera = cv2.VideoCapture(args["video"])
 
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('boatvideo5.avi', fourcc, 20.0, (640, 480))
+
+xGreen = 0
+yGreen = 0
+xRed = 0
+yRed = 0
+i = 0;
 
 # keep looping
 while True:
-    camera.set(15, -10)
     # grab the current frame
     (grabbed, frame) = camera.read()
 
@@ -64,7 +81,7 @@ while True:
         break
 
     # resize the frame, blur it, and convert it to the HSV color space
-    frame = imutils.resize(frame, width=1080)
+    frame = imutils.resize(frame, width=640, height=480)
 
     # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -106,11 +123,11 @@ while True:
         centerGreen = (int(MGreen["m10"] / MGreen["m00"]), int(MGreen["m01"] / MGreen["m00"]))
 
         # only proceed if the radius meets a minimum size
-        if radiusGreen > 30:
+        if radiusGreen > 10:
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
             cv2.circle(frame, (int(xGreen), int(yGreen)), int(radiusGreen),
-                       (0, 0, 255), -1)
+                       (0, 255, 255), 2)
             cv2.circle(frame, centerGreen, 5, (0, 255, 0), -1)
         else:
             xGreen = 0
@@ -131,12 +148,66 @@ while True:
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
             cv2.circle(frame, (int(xRed), int(yRed)), int(radiusRed),
-                        (0, 0, 255), -1)
+                        (0, 255, 255), 2)
             cv2.circle(frame, centerRed, 5, (0, 0, 255), -1)
         else:
             xRed = 0
             yRed = 0
 
+    mX = 0
+    mY = 0
+
+    # draw circle at midpoint
+    if (xRed != 0) and (xGreen != 0):
+        # find midpoint of line
+        mX = int(xRed + xGreen) / 2
+        mY = int(yRed + yGreen) / 2
+        cv2.circle(frame, (mX, mY), 20, (255, 0, 255), -1)
+
+    # showing position
+    cv2.putText(frame, "Green Ball",
+                (10, 280),
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+    cv2.putText(frame, str(int(xGreen)) + "," + str(int(yGreen)),
+                (10, 320),
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+
+    cv2.putText(frame, "Red Ball",
+                (140, 280),
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+
+    cv2.putText(frame, str(int(xRed)) + "," + str(int(yRed)),
+                (140, 320),
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+
+    cv2.putText(frame, "Center",
+                (260, 280),
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+
+    cv2.putText(frame, str(mX) + "," + str(mY),
+                (260, 320),
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+
+    cv2.circle(frame, (200, 230), 20, green, thickness=-1)
+    cv2.circle(frame, (79, 230), 20, red, thickness=-1)
 
 
     # update the points queue
@@ -171,6 +242,11 @@ while True:
     # show the frame to our screen
     cv2.imshow("Frame", frame)
 
+    out.write(frame)
+    cv2.imwrite('roboBoat'+str(i)+'.jpg', frame)
+
+    i += 1
+
     key = cv2.waitKey(1) & 0xFF
     # if the 'q' key is pressed, stop the loop
     if key == ord("q"):
@@ -178,4 +254,5 @@ while True:
 
 # cleanup the camera and close any open windows
 camera.release()
+out.release()
 cv2.destroyAllWindows()
